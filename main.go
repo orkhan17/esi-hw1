c31a254a72a0d17838d173fe846a891ef6f9af23
Orkhan Jamaladdinov <orxan.camaleddinov@gmail.com>
2021-03-03 23:01:30 +0200

    Added unique feature

--- main.go
+++ main.go
@@ -14,12 +14,13 @@ import (
 
 // ToDo ...
 type ToDo struct {
-	ID          string `json:"Id"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
-	Date        string `json:"Date"`
-	Time        string `json:"Time"`
-	Completed   bool   `json:"Completed"`
+	ID           string `json:"Id"`
+	Title        string `json:"Title"`
+	Description  string `json:"Description"`
+	Date         string `json:"Date"`
+	Time         string `json:"Time"`
+	HighPriority bool   `json:"HighPriority"`
+	Completed    bool   `json:"Completed"`
 }
 
 // ToDos ...
@@ -70,6 +71,7 @@ func handleRequests() {
 	myRouter.HandleFunc("/todo", createNewToDo).Methods("POST")
 	myRouter.HandleFunc("/todo/{id}", deleteToDo).Methods("DELETE")
 	myRouter.HandleFunc("/todo/{id}", updateToDo).Methods("PUT")
+	myRouter.HandleFunc("/todos/{id}", updatePriorityToDo).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
@@ -117,6 +119,31 @@ func updateToDo(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(td)
 }
 
+func updatePriorityToDo(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	key := 0
+	for index, todo := range ToDos {
+		if todo.ID == id {
+			key = index
+		}
+	}
+
+	var td ToDo = ToDos[key]
+
+	td.HighPriority = true
+
+	for index, todo := range ToDos {
+		if todo.ID == id {
+			ToDos = append(ToDos[:index], ToDos[index+1:]...)
+		}
+	}
+
+	ToDos = append(ToDos, td)
+
+	json.NewEncoder(w).Encode(td)
+}
+
 func createNewToDo(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var todo ToDo
@@ -141,23 +168,26 @@ func deleteToDo(w http.ResponseWriter, r *http.Request) {
 func main() {
 	ToDos = []ToDo{
 		ToDo{ID: "1",
-			Title:       "Meeting with friends",
-			Description: "Meeting with my friends in city center",
-			Date:        "2021-03-06",
-			Time:        "2:00 pm",
-			Completed:   false},
+			Title:        "Meeting with friends",
+			Description:  "Meeting with my friends in city center",
+			Date:         "2021-03-06",
+			Time:         "2:00 pm",
+			HighPriority: true,
+			Completed:    false},
 		ToDo{ID: "2",
-			Title:       "Shopping",
-			Description: "Don't forget to buy clothes",
-			Date:        "2021-03-06",
-			Time:        "4:00 pm",
-			Completed:   true},
+			Title:        "Shopping",
+			Description:  "Don't forget to buy clothes",
+			Date:         "2021-03-06",
+			Time:         "4:00 pm",
+			HighPriority: true,
+			Completed:    true},
 		ToDo{ID: "3",
-			Title:       "Watch football",
-			Description: "Watch Champions League Matches",
-			Date:        "2021-03-09",
-			Time:        "10:00 pm",
-			Completed:   true},
+			Title:        "Watch football",
+			Description:  "Watch Champions League Matches",
+			Date:         "2021-03-09",
+			Time:         "10:00 pm",
+			HighPriority: false,
+			Completed:    true},
 	}
 	handleRequests()
 }
